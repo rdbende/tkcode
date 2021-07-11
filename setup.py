4a3d00d255b8df7f2422eb6c29ffc92c7cb8322a
rdbende <rdbende@gmail.com>
2021-07-11 11:25:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup
+import subprocess
+
+version = subprocess.check_output(["git", "describe", "--abbrev=0", "--tags"]).strip().decode()
+
+assert version[0] == "v"  # Something went wrong
+
+with open("README.md", "r") as file:
+    long_description = file.read()
 
 setup(
     name="tkcode",
-    version="0.1",
+    version=version,
     description="A code editor and code block widget for tkinter with syntax highlighting, and some other useful stuff",
+    long_description=long_description,
     author="rdbende",
     author_email="rdbende@gmail.com",
     url="https://github.com/rdbende/tkcode",
     install_requires=['pygments'],
     python_requires='>=3.6',
     packages=["tkcode"],
-    package_data={"tkcode": ["schemes/*"]}
+    package_data={"tkcode": ["schemes/*"]},
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
